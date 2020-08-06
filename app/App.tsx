95ec4f88c9dbe0016ffb25cde9ffaf7fe1d1d312
kazuyuki takahashi <kazrbb@gmail.com>
2020-08-07 00:41:44 +0900

    aaa

--- app/App.tsx
+++ app/App.tsx
@@ -1,5 +1,6 @@
+import "react-native-gesture-handler"
 import * as firebase from "firebase"
-import firebaseConfig from './firebaseConfig'
+import firebaseConfig from "./firebaseConfig"
 import { App } from "./src/App"
 
 // Initialize Firebase
--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,13 +1,32 @@
 import React from "react"
 import { View, Text, StyleSheet } from "react-native"
 import { StatusBar } from "expo-status-bar"
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
-export function App() {
+function Home() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  )
+}
+
+function Settings() {
+  return <Text>Settings</Text>
+}
+
+const Tab = createBottomTabNavigator()
+
+export function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
 
