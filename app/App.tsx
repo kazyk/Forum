e55b2c4873ef7102bac61e7c8cf59d8265ec6457
kazuyuki takahashi <kazrbb@gmail.com>
2020-08-07 00:10:46 +0900

    nnn

---
+++ app/App.tsx
@@ -0,0 +1,8 @@
+import * as firebase from "firebase"
+import firebaseConfig from './firebaseConfig'
+import { App } from "./src/App"
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+export default App
