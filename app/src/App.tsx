a7171d08c674ba8f240776d92e162ad239a519b4
kazuyuki takahashi <kazrbb@gmail.com>
2020-08-07 22:34:47 +0900

    Update App.tsx

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,6 +3,9 @@ import { View, Text, StyleSheet } from "react-native"
 import { StatusBar } from "expo-status-bar"
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { createStackNavigator } from '@react-navigation/stack'
+
+const HomeStack = createStackNavigator()
 
 function Home() {
   return (
@@ -10,6 +13,14 @@ function Home() {
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  )
+}
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={Home} />
+    </HomeStack.Navigator>
   )
 }
 
@@ -31,7 +42,7 @@ export function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Home" component={HomeStackScreen} />
         <Tab.Screen name="Communities" component={Communities} />
         <Tab.Screen name="Search" component={Search} />
         <Tab.Screen name="Settings" component={Settings} />
